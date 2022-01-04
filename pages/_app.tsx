fedd6c4e770307d1334150db0f9b8fe40e8415b2
tal7aouy <talhaouycoding@gmail.com>
2022-01-04 22:50:41 +0100

    design property component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
-import '../styles/globals.css'
+import Router from 'next/router'
+import Head from 'next/head'
+import NProgress from 'nprogress'
+import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
-
+// import Layout from '../components/Layout'
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
