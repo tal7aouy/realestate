b01cb7c5b51e22f2824e862ba92a6374cecc8114
tal7aouy <talhaouycoding@gmail.com>
2022-01-08 20:31:22 +0100

    add: search by properties

--- components/SearchFilters.tsx
+++ components/SearchFilters.tsx
@@ -12,10 +12,40 @@ import {
 import { useRouter } from 'next/router'
 import { MdCancel } from 'react-icons/md'
 import Image from 'next/image'
+import { filterData, getFilterValues } from '../utils/filter'
+
 function SearchFilters() {
-  const [filters, setFilters] = useState({})
+  const [filters, setFilters] = useState(filterData)
+  const router = useRouter()
+  const searchProperties = (filterValues: any) => {
+    const path = router.pathname
+    const { query } = router
+    const values = getFilterValues(filterValues)
+    values.forEach((element: any) => {
+      query[element.name] = element.value
+    })
+    router.push({ pathname: path, query })
+  }
   return (
-    <Flex bg='gray.100' p='4' flexWra='wrap' justifyContent='center'></Flex>
+    <Flex bg='gray.100' p='4' flexWra='wrap' justifyContent='center'>
+      {filters.map((filter: any) => (
+        <Box key={filter.queryName}>
+          <Select
+            placeholder={filter.placeholder}
+            w='fit-content'
+            onChange={(e) =>
+              searchProperties({ [filter.queryName]: e.target.value })
+            }
+          >
+            {filter?.items?.map((op: any) => (
+              <option key={op} value={op.value}>
+                {op.name}
+              </option>
+            ))}
+          </Select>
+        </Box>
+      ))}
+    </Flex>
   )
 }
 
